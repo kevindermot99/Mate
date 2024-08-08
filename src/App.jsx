cbbe985ff859e3c4dae26b26de99c0c8a651e055
kevindermot99 <mbonimpayekevin@gmail.com>
2024-08-08 10:01:38 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Converter from './components/Converter'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <div>
+      <Navbar />
       <Converter />
     </div>
   )
