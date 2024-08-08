ae0d2eac00c733b248b96eaba0d6ba84259faf63
kevindermot99 <mbonimpayekevin@gmail.com>
2024-08-08 10:01:41 +0200

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import logo from '../assets/logo.png'
 
 function Navbar() {
   return (
     <div className='w-full h-[60px] flex items-center justify-center'>
+        <img src={logo} alt="" />
         <h1 className=''>Money<span>Mate</span></h1>
     </div>
   )
