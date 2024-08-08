7cd91f23e88d154218df037b1a59fedcd58c178c
kevindermot99 <mbonimpayekevin@gmail.com>
2024-08-08 09:17:29 +0200

    Create Navbar.jsx

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='w-full h-[60px] flex items-center justify-center'>
+        <h1 className=''>Money<span>Mate</span></h1>
+    </div>
+  )
+}
+
+export default Navbar
