9e01af781718360cd9ea224af2f0001f7ced43b7
kevindermot99 <mbonimpayekevin@gmail.com>
2024-08-08 09:36:58 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+      },
+      colors: {
+        "light-body": "#F1EFE7",
+        "dark-body": "#0a0a0a",
+        "main-color": "#f97316"
+      },
+    },
   },
   plugins: [],
-}
-
+};
