cd7ba17814ae1f31fccd9b662f6b290b715d3d1f
kevindermot99 <mbonimpayekevin@gmail.com>
2024-08-08 09:56:21 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,12 @@ export default {
       fontFamily: {
       },
       colors: {
-        "light-body": "#F1EFE7",
-        "dark-body": "#0a0a0a",
-        "main-color": "#f97316"
+        "main-color": "#fe5707",
+        "main-color-light":"#FBAC1D",
+        "body-color":"#090a0a",
+        "inactive-container":"#0F1010",
+        "active-container":"#191919",
+        "text-color":"#ffffff",
       },
     },
   },
