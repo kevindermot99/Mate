ad740116284b6e2a0593d7180e6ae0ada1e6b862
kevindermot99 <mbonimpayekevin@gmail.com>
2024-08-08 10:51:45 +0200

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import logo from '../assets/logo.png'
 
 function Navbar() {
     return (
-        <div className='w-full h-[60px] flex items-baseline justify-center p-5 gap-1 absolute top-0 left-0 bg-light-body dark:bg-dark-body-color'>
+        <div className='w-full h-[60px] flex items-baseline justify-center p-5 gap-1'>
             <div className='min-w-[30px]'><img src={logo} className="h-5" /></div>
             <h1 className='font-medium text-xl'>Money<span>Mate</span></h1>
         </div>
