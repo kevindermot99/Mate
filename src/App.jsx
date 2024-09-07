e2ef3611b9d4b620c5cdc0a43680b3f9f8d1cc6b
kevindermot99 <mbonimpayekevin@gmail.com>
2024-09-07 12:26:17 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Home from './pages/Home'
 import { HashRouter, Route, Routes } from 'react-router-dom'
+import ToolsAndTechs from './pages/ToolsAndTechs'
 
 function App() {
   return (
     <HashRouter>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/tools_techs' element={<ToolsAndTechs />} />
       </Routes>
     </HashRouter>
   )
