be75d8b4764d18f1ebdbfe6d478d2a7233673f46
kevindermot99 <mbonimpayekevin@gmail.com>
2024-09-07 12:26:49 +0200

    Create ToolsAndTechs.jsx

---
+++ src/pages/ToolsAndTechs.jsx
@@ -0,0 +1,16 @@
+import React, { useEffect, useRef, useState } from "react";
+import StickyNav from "../components/StickyNav";
+
+function ToolsAndTechs() {
+
+
+  return (
+    <div className="flex flex-col h-full min-h-svh w-full">
+      {/* Sticky nav */}
+      <StickyNav />
+      tools_techs
+    </div>
+  );
+}
+
+export default ToolsAndTechs;
